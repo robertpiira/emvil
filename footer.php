3336741b1aaf478a8d4d5b6111a7f1223fd66458
robertpiira <robert@piira.se>
2011-03-09 21:59:52 +0100

    getting the media queries in shape

--- footer.php
+++ footer.php
@@ -20,23 +20,21 @@
 	get_sidebar( 'footer' );
 ?>
 
-			<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
-			<a href="http://www.piira.se" title="Robert Piirainen, webbdesigner, gränssnitsutvecklare" >Design, HTML &amp; CSS by Piira design</a>
+<ul class="footer-links">
+	<li>
+		<a href="http://www.piira.se" title="Robert Piirainen, webbdesigner, gränssnittsutvecklare" >Themed by Robert Piira</a>
+	</li>
+	<li>
+		<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
+	</li>
+</ul>
 
-<?php
-	/* Always have wp_footer() just before the closing </body>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to reference JavaScript files.
-	 */
 
-	wp_footer();
-?>
 </footer>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/emvil.js"></script>
-
+<?php wp_footer(); ?>
 
 </body>
 </html>
